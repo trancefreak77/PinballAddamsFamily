1a8e860ba27e368939f5c0b753e20856110764c8
trancefreak77 <christian.valentin@tele2.at>
2015-01-14 21:38:28 +0100

    Updated

--- lib/Game/PinballHSM.cpp
+++ lib/Game/PinballHSM.cpp
@@ -19,152 +19,78 @@ void PinballHSM::onBumper1SwitchClosed() {}
 
 void PinballHSM::onBumper1SwitchOpened() {}
 
-void PinballHSM::onBumper2SwitchClosed() {
+void PinballHSM::onBumper2SwitchClosed() {}
 
-}
+void PinballHSM::onBumper2SwitchOpened() {}
 
-void PinballHSM::onBumper2SwitchOpened() {
+void PinballHSM::onBumper3SwitchClosed() {}
 
-}
+void PinballHSM::onBumper3SwitchOpened() {}
 
-void PinballHSM::onBumper3SwitchClosed() {
+void PinballHSM::onBumper4SwitchClosed() {}
 
-}
+void PinballHSM::onBumper4SwitchOpened() {}
 
-void PinballHSM::onBumper3SwitchOpened() {
+void PinballHSM::onBumper5SwitchClosed() {}
 
-}
+void PinballHSM::onBumper5SwitchOpened() {}
 
-void PinballHSM::onBumper4SwitchClosed() {
+void PinballHSM::onLeftSlingshotSwitchClosed() {}
 
-}
+void PinballHSM::onLeftSlingshotLeftSwitchOpened() {}
 
-void PinballHSM::onBumper4SwitchOpened() {
+void PinballHSM::onRightSlingshotSwitchClosed() {}
 
-}
+void PinballHSM::onRightSlingshotSwitchOpened() {}
 
-void PinballHSM::onBumper5SwitchClosed() {
+void PinballHSM::onShooterLanePlungerSwitchClosed() {}
 
-}
+void PinballHSM::onShooterLanePlungerSwitchOpened() {}
 
-void PinballHSM::onBumper5SwitchOpened() {
+void PinballHSM::onShooterLaneRampSwitchClosed() {}
 
-}
+void PinballHSM::onShooterLaneRampSwitchOpened() {}
 
-void PinballHSM::onLeftSlingshotSwitchClosed() {
+void PinballHSM::onOuterLoopRightSwitchClosed() {}
 
-}
+void PinballHSM::onOuterLoopRightSwitchOpened() {}
 
-void PinballHSM::onLeftSlingshotLeftSwitchOpened() {
+void PinballHSM::onOuterLoopLeftSwitchClosed() {}
 
-}
+void PinballHSM::onOuterLoopLeftSwitchOpened() {}
 
-void PinballHSM::onRightSlingshotSwitchClosed() {
+void PinballHSM::onThingRampSwitchClosed() {}
 
-}
+void PinballHSM::onThingRampSwitchOpened() {}
 
-void PinballHSM::onRightSlingshotSwitchOpened() {
+void PinballHSM::onThingHoleSwitchClosed() {}
 
-}
+void PinballHSM::onThingHoleSwitchOpened() {}
 
-void PinballHSM::onShooterLanePlungerSwitchClosed() {
+void PinballHSM::onFarLeftInlaneSwitchClosed() {}
 
-}
+void PinballHSM::onFarLeftInlaneSwitchOpened() {}
 
-void PinballHSM::onShooterLanePlungerSwitchOpened() {
+void PinballHSM::onLeftOutlaneSwitchClosed() {}
 
-}
+void PinballHSM::onLeftOutlaneSwitchOpened() {}
 
-void PinballHSM::onShooterLaneRampSwitchClosed() {
+void PinballHSM::onLeftInnerInlaneSwitchClosed() {}
 
-}
+void PinballHSM::onLeftInnerInlaneSwitchOpened() {}
 
-void PinballHSM::onShooterLaneRampSwitchOpened() {
+void PinballHSM::onRightInlaneSwitchClosed() {}
 
-}
+void PinballHSM::onRightInlaneSwitchOpened() {}
 
-void PinballHSM::onOuterLoopRightSwitchClosed() {
+void PinballHSM::onRightOutlaneSwitchClosed() {}
 
-}
+void PinballHSM::onRightOutlaneSwitchOpened() {}
 
-void PinballHSM::onOuterLoopRightSwitchOpened() {
+void PinballHSM::onLeftFlipperButtonSwitchClosed() {}
 
-}
+void PinballHSM::onLeftFlipperButtonSwitchOpened() {}
 
-void PinballHSM::onOuterLoopLeftSwitchClosed() {
-
-}
-
-void PinballHSM::onOuterLoopLeftSwitchOpened() {
-
-}
-
-void PinballHSM::onThingRampSwitchClosed() {
-
-}
-
-void PinballHSM::onThingRampSwitchOpened() {
-
-}
-
-void PinballHSM::onThingHoleSwitchClosed() {
-
-}
-
-void PinballHSM::onThingHoleSwitchOpened() {
-
-}
-
-void PinballHSM::onFarLeftInlaneSwitchClosed() {
-
-}
-
-void PinballHSM::onFarLeftInlaneSwitchOpened() {
-
-}
-
-void PinballHSM::onLeftOutlaneSwitchClosed() {
-
-}
-
-void PinballHSM::onLeftOutlaneSwitchOpened() {
-
-}
-
-void PinballHSM::onLeftInnerInlaneSwitchClosed() {
-
-}
-
-void PinballHSM::onLeftInnerInlaneSwitchOpened() {
-
-}
-
-void PinballHSM::onRightInlaneSwitchClosed() {
-
-}
-
-void PinballHSM::onRightInlaneSwitchOpened() {
-
-}
-
-void PinballHSM::onRightOutlaneSwitchClosed() {
-
-}
-
-void PinballHSM::onRightOutlaneSwitchOpened() {
-
-}
-
-void PinballHSM::onLeftFlipperButtonSwitchClosed() {
-
-}
-
-void PinballHSM::onLeftFlipperButtonSwitchOpened() {
-
-}
-
-void PinballHSM::onRightFlipperButtonSwitchClosed() {
-
-}
+void PinballHSM::onRightFlipperButtonSwitchClosed() {}
 
 void PinballHSM::onRightFlipperButtonSwitchOpened() {}
